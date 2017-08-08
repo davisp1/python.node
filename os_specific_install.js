9cf5de2412209ac0e4ce8c51f05de932653e7f47
Reese Perry <jreesep@gmail.com>
2017-08-07 19:57:55 -0500

    testing some fun stuff

--- os_specific_install.js
+++ os_specific_install.js
@@ -1,11 +1,15 @@
+var python = require('python.node')
 var os = require('os')
 var execSync = require('child_process').execSync;
 
-
-if (os.platform() === 'darwin') {
-	try {
+def os_picker():
+	if os.platform() === 'darwin':
+		try:
+			execSync('node-gyp rebuild', {stdio:[0,1,2]});
+		catch (err):
+			process.exit(err.status);
+	elif os.platform() == 'win':
+		try:
 		execSync('node-gyp rebuild', {stdio:[0,1,2]});
-	} catch (err) {
+	catch (err):
 		process.exit(err.status);
-	}
-}
