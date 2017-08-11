48db758d080df86cc8a099107de80066b5a277ff
Reese Perry <jreesep@gmail.com>
2017-08-11 11:13:22 -0500

    operation windows

--- cflags.py
+++ cflags.py
@@ -2,7 +2,7 @@ import sysconfig
 import subprocess
 
 def cflags():
-    sysconfig.get_config_var('cflags')
-    print(sysconfig.get_paths()['include'])
+    sysconfig.get_config_var('include_dirs')
+    print(sysconfig.get_paths()['include'] + '\\python.h')
 
 cflags()
