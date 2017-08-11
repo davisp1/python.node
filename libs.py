f83fed890ce122f78415c2e8ca6edd86ea673223
Reese Perry <jreesep@gmail.com>
2017-08-11 11:50:38 -0500

    operation windows

--- libs.py
+++ libs.py
@@ -3,6 +3,6 @@ import subprocess
 
 def libraries():
     sysconfig.get_config_var('libraries')
-    print(sysconfig.get_paths()['stdlib'] + ' ..\\libs' + '\\python' + sysconfig.get_config_vars()['py_version_nodot'] + '.lib')
+    print(sysconfig.get_paths()['data'] + '\\libs\\python' + sysconfig.get_config_vars()['py_version_nodot'] + '.lib')
 
 libraries()
